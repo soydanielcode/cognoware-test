261ab83db6325fc7f3fb4bd3ffbe24a3a7978e6c
soydanielcode <soydanielcode@outlook.com>
2024-01-23 14:06:39 -0500

    Init Project add src

---
+++ src/main/java/com/cognoware/test/TestCognowareApplication.java
@@ -0,0 +1,13 @@
+package com.cognoware.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCognowareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCognowareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognoware/test/TestCognowareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognoware.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCognowareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
