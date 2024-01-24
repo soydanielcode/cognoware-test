368b7236f9a6a679ad95d6d9d473fd8f430e22e9
soydanielcode <soydanielcode@outlook.com>
2024-01-24 09:16:46 -0500

    Add new implementation

--- webapp/test-cognoware/src/app/app.routes.ts
+++ webapp/test-cognoware/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { CrudComponent } from './pages/crud/crud.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'report',
+        component: ReportsComponent
+    },
+    {
+        path: 'crud',
+        component: CrudComponent
+    }
+];
